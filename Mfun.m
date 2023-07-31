ba51d96d799b0fea09a4d18ba9f9627c00c6f8e8
Nolan Canegallo <92232664+nacanega@users.noreply.github.com>
2023-07-31 09:31:31 -0400

    Delete Mfun.m

--- Mfun.m
+++
@@ -1,5 +0,0 @@
-function [mass] = Massfun(lc,h0,h1,h2,r1,r3,r4)
-rho_cu = 8960;          % kg/m^3
-rho_fe = 7874;          % kg/m^3
-mass = pi*rho_cu*0.25*d^2*lc + pi*rho_fe*(h0.*(r1.^2 - r0.^2) + 2*h1.*(r3.^2 - r0.^2) + 2*h2.*(r4.^2 - r3.^2));
-end
